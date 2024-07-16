df020f3c78612c56a366171258a0d0cac2d3d8f9
Ronald Weigan <roanldweigan2@gmail.com>
2024-07-16 02:38:40 -0300

    separating buttons into components

---
+++ src/pages/components/button.tsx
@@ -0,0 +1,14 @@
+import { Settings2 } from "lucide-react";
+import { ReactNode } from "react";
+
+interface ButtonProps {
+    children: ReactNode
+}
+
+export function Button({children} : ButtonProps) {
+    return (
+        <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+            {children}
+        </button>
+    )
+}
--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -51,5 +51,3 @@ export function TripDetailsPage() {
         </div>
     )
 }
-
-// MINUTO 44
